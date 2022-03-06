5d6c5de9eabc4a5138dc6951873e71d3f2271ff6
Habiba.Ahmed <habiba.hamad97@eng-st.cu.edu.eg>
2022-03-07 01:33:17 +0200

    Fix RunTime Error
    Signed-off-by: Habiba.Ahmed <habiba.hamad97@eng-st.cu.edu.eg>

--- src/main/java/com/java/main/controller/request/LocationRequest.java
+++ src/main/java/com/java/main/controller/request/LocationRequest.java
@@ -1,10 +1,14 @@
 package com.java.main.controller.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class LocationRequest {
 	private String name;
 	private boolean temp;
